51cf968444a6de21aff81273c40096168a6f6816
Ben Hall <bentomhall@gmail.com>
2018-03-12 22:36:39 -0400

    Added skeleton of export code

---
+++ DMPrepHelper/Export/ExportWriter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace DMPrepHelper.Export
+{
+    public interface IExporter<T>
+    {
+        string Marshal(IEnumerable<T> t);
+    }
+
+    public class ExportWriter
+    {
+        private IStorageFile file;
+
+        public ExportWriter(IStorageFile storageFile)
+        {
+            file = storageFile;
+        }
+
+        public async Task WriteFile<T>(IExporter<T> exporter, IEnumerable<T> data)
+        {
+            var contents = exporter.Marshal(data);
+            await FileIO.WriteTextAsync(file, contents);
+        }
+    }
+}
---
+++ DMPrepHelper/Export/Exporters.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibGenerator.NPC;
+using LibGenerator.Settlement;
+using LibGenerator.Dungeon;
+using Windows.Storage;
+
+namespace DMPrepHelper.Export
+{
+    public class ExportBase<T> : IExporter<T>
+    {
+        public string Marshal(IEnumerable<T> t)
+        {
+            string separator = $"{Environment.NewLine}------------------------{Environment.NewLine}";
+            var data = t.Select(x => FormatData(x));
+            return string.Join(separator, data);
+        }
+
+        // dummy implementation
+        string FormatData(object obj) { return ""; }
+    }
+
+
+    public class PersonExporter : ExportBase<PersonData>
+    {
+        private string FormatData(PersonData d)
+        {
+            var output = new List<string>()
+            {
+                $"Name: {d.Name}"
+            };
+            if (string.IsNullOrEmpty(d.Subrace))
+            {
+                output.Add($"Demographics: {d.Age} {d.Gender} {d.Race} from {d.Nation} ({d.Culture})");
+            }
+            else
+            {
+                output.Add($"Demographics: {d.Age} {d.Gender} {d.Race} ({d.Subrace}) from {d.Nation} ({d.Culture})");
+            }
+            output.Add($"Profession: {d.Profession}");
+            output.Add($"Religious habits: {d.Religion}");
+            output.Add($"Personality: {d.Personality}");
+            return string.Join(Environment.NewLine, output);
+        }
+    }
+
+    public class DungeonExporter : ExportBase<AdventureData>
+    {
+        private string FormatData(AdventureData d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SettlementExporter : ExportBase<SettlementData>
+    {
+        private string FormatData(SettlementData d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
