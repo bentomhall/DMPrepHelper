42878c0c15b49d1c603bf662577243df11c4ebc7
Ben Hall <bentomhall@gmail.com>
2018-03-23 22:19:29 -0400

    Began adding culture config pages

--- DMPrepHelper/ViewModels/CultureConfigViewModel.cs
+++ DMPrepHelper/ViewModels/CultureConfigViewModel.cs
@@ -9,15 +9,40 @@ using LibGenerator.NPC;
 
 namespace DMPrepHelper.ViewModels
 {
-    class CultureConfigViewModel : ConfigBaseViewModel
+    public class CultureConfigViewModel : ConfigBaseViewModel
     {
         private List<CultureData> storedData;
+        private string nation;
+        private string race;
+        private ObservableCollection<Tuple<string, string>> displayItems;
+        private ObservableCollection<string> cultures;
+        private ObservableCollection<DictItem> genders;
+        private ObservableCollection<string> religions;
+        private ObservableCollection<DictItem> subraces;
+
+        private Tuple<string, string> selectedItem;
+        private string selectedCulture;
+        private DictItem selectedGender;
+        private string selectedReligion;
+        private DictItem selectedSubrace;
 
         public CultureConfigViewModel(StorageHelper s) : base(s)
         {
             storedData = GetData();
             items = new ObservableCollection<string>(storedData.Select(x => x.Race));
+            displayItems = new ObservableCollection<Tuple<string, string>>(storedData.Select(x => new Tuple<string, string>(x.Race, x.Nation)));
         }
+
+        public string Race { get => race; set => SetProperty(ref race, value); }
+        public string Nation { get => nation; set => SetProperty(ref nation, value); }
+        public ObservableCollection<Tuple<string, string>> ItemNames { get => displayItems; set => SetProperty(ref displayItems, value); }
+        public ObservableCollection<string> Cultures { get => cultures; set => SetProperty(ref cultures, value); }
+        public ObservableCollection<DictItem> Genders { get => genders; set => SetProperty(ref genders, value); }
+        public ObservableCollection<DictItem> Subraces { get => subraces; set => SetProperty(ref subraces, value); }
+        public ObservableCollection<string> Religions { get => religions; set => SetProperty(ref religions, value); }
+
+        public Tuple<string, string> SelectedItem { get => selectedItem; set => SetProperty(ref selectedItem, value); }
+        public string SelectedCulture { get => selectedCulture; set => SetProperty(ref selectedCulture, value); }
 
         private List<CultureData> GetData()
         {
@@ -51,7 +76,13 @@ namespace DMPrepHelper.ViewModels
 
         protected override void DidSelectItem(string p)
         {
-            base.DidSelectItem(p);
+            var item = storedData.First(x => (x.Race == selectedItem.Item1) && (x.Nation == selectedItem.Item2));
+            Race = item.Race;
+            Nation = item.Nation;
+            Cultures = new ObservableCollection<string>(item.Culture);
+            Genders = new ObservableCollection<DictItem>(item.Gender.Select(x => new DictItem(x)));
+            Subraces = new ObservableCollection<DictItem>(item.Subrace.Select(x => new DictItem(x)));
+            Religions = new ObservableCollection<string>(item.Religiosity);
         }
     }
 }
---
+++ DMPrepHelper/Views/CultureConfigPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DMPrepHelper.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CultureConfigPage : Page
+    {
+        private ViewModels.CultureConfigViewModel model;
+        private StorageHelper storage;
+
+        public CultureConfigPage()
+        {
+            this.InitializeComponent();
+            storage = ((App)Application.Current).Storage;
+            ViewModel = new ViewModels.CultureConfigViewModel(storage);
+        }
+
+        public ViewModels.CultureConfigViewModel ViewModel { get => model; set => model = value; }
+
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var clicked = e.ClickedItem as string;
+            ViewModel.SelectItemCommand.Execute(clicked);
+        }
+    }
+}
