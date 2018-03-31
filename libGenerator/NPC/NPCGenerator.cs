1d8727b72818f07c951d045c86d642b9410d444f
Ben Hall <bentomhall@gmail.com>
2018-03-31 12:26:27 -0400

    scaffolding for nation generation

--- libGenerator/NPC/NPCGenerator.cs
+++ libGenerator/NPC/NPCGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LibGenerator.Core;
+using LibGenerator.Nation;
 
 namespace LibGenerator.NPC
 {
---
+++ libGenerator/Nation/PolityData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LibGenerator.NPC;
+using LibGenerator.Settlement;
+
+namespace libGenerator.Nation
+{
+    public class PolityData
+    {
+        public string Name { get; set; } = "";
+        public string GovernmentName { get; set; } = "";
+        public List<PersonData> Leaders { get; set; } = new List<PersonData>();
+        public List<Settlement> MajorSettlements { get; set; } = new List<Settlement>();
+        public int Population { get; set; } = 0;
+        public Dictionary<string, double> Races { get; set; } = new Dictionary<string, double>();
+        public string EconomicSystem { get; set; } = "";
+        public string ForeignPolicy { get; set; } = "";
+        public string SocialAttitude { get; set; } = "";
+        public string LandArea { get; set; } = "";
+    }
+}
